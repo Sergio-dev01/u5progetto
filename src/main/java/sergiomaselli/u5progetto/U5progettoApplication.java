3dc889b388e4777fe05bfff4c24a0719e5e9d2c1
Sergio-dev01 <sergio.develoop@gmail.com>
2025-07-18 10:43:48 +0200

    Initial commit

---
+++ src/main/java/sergiomaselli/u5progetto/U5progettoApplication.java
@@ -0,0 +1,13 @@
+package sergiomaselli.u5progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5progettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5progettoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sergiomaselli/u5progetto/U5progettoApplicationTests.java
@@ -0,0 +1,13 @@
+package sergiomaselli.u5progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5progettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
